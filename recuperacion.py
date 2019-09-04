b3e515194af897955b6234eda8f70b5f5ff244d1
defcxz <mario6632@gmail.com>
2019-09-04 19:40:52 -0400

    Comentarios quitados

--- recuperacion.py
+++ recuperacion.py
@@ -1,9 +1,8 @@
-"""
 r = float(input("Ingrese el radio: "))
 h = float(input("Ingrese la altura: "))
 
 area = 2*3.14*r*h
 volumen = 3.14*r*r*h
 
-print("El área del cilindro es: ", area, "y su volumen es: ",volumen)
-"""
+print("El área del cilindro es: ", area, " y su volumen es: ",volumen)
+
